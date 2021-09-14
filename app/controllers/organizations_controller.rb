c30df910677b7cf9e1e627167414924bd5b818a7
yukisa2010 <yukisa2010@gmail.com>
2021-09-14 12:15:22 +0900

    Add create actions both

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -23,7 +23,7 @@ class OrganizationsController < ApplicationController
     private
 
         def organization_params
-            params.require(:organization).permit(:name)
+            params.permit(:name)
         end
 
 end
